45b38f85840e5b53ec4ee72e01634d9b61052174
ferzman720 <71725110+ferzman720@users.noreply.github.com>
2020-10-22 13:58:45 -0500

    Add files via upload

---
+++ Arreglos/cmake-build-debug/CMakeFiles/PracticasComputacionI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PracticasComputacionI.dir/main.cpp.o"
+  "PracticasComputacionI"
+  "PracticasComputacionI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PracticasComputacionI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
