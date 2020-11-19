1e1f7b393889899f965ad18e8758abc84fb85b69
ferzman720 <71725110+ferzman720@users.noreply.github.com>
2020-11-18 18:30:41 -0600

    Add files via upload

---
+++ CargaElectrica/cmake-build-debug/CMakeFiles/CargaElectrica.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CargaElectrica.dir/main.cpp.o"
+  "CargaElectrica"
+  "CargaElectrica.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CargaElectrica.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
