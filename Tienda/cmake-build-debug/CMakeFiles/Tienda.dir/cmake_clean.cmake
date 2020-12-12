b7972645a745f23a7a6b0bd3493e6c45c081bbc9
ferzman720 <71725110+ferzman720@users.noreply.github.com>
2020-12-11 18:54:23 -0600

    Add files via upload

---
+++ Tienda/cmake-build-debug/CMakeFiles/Tienda.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tienda.dir/Almacen.cpp.o"
+  "CMakeFiles/Tienda.dir/main.cpp.o"
+  "Tienda"
+  "Tienda.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tienda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
